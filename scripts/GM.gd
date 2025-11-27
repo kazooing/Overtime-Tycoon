0d181241ad14189e1aac8cf398204ac247b3740a
kazooing <amiridanish26@gmail.com>
2025-11-27 18:06:04 +0700

    feat: added some sprites and added how to do certain tasks

--- scripts/GM.gd
+++ scripts/GM.gd
@@ -45,7 +45,7 @@ var tasks = [
 	{
 		"id" = "000",
 		"nama" = "telephone",
-		"icon" = "res://sprites1/work_scene/Phone neutral.png",
+		"icon" = "res://sprites1/upgrade_shop/phone.png",
 		"upgrade_value" = [7, 9, 12, 15],
 		"upgrade_cost" = [5, 10, 15, 20],
 		"owned" = true,
@@ -54,7 +54,7 @@ var tasks = [
 	{
 		"id" = "001",
 		"nama" = "spreadsheet",
-		"icon" = "res://sprites1/work_scene/screen/sp1.png",
+		"icon" = "res://sprites1/upgrade_shop/excell.png",
 		"upgrade_value" = [9, 12, 16, 21],
 		"upgrade_cost" = [7, 13, 19, 25],
 		"owned" = false,
@@ -64,7 +64,7 @@ var tasks = [
 	{
 		"id" = "002",
 		"nama" = "meeting",
-		"icon" = "res://sprites/icon.svg",
+		"icon" = "res://sprites1/work_scene/webcam put on top of monitor.png",
 		"upgrade_value" = [25, 30, 35, 40],
 		"upgrade_cost" = [18, 22, 26, 30],
 		"owned" = false,
--- scripts/shop/task_script.gd
+++ scripts/shop/task_script.gd
@@ -3,6 +3,7 @@ extends TextureButton
 
 @export var task_name: String
 @export var task_description: String
+@export var task_to_use: String
 @onready var UIlayer = get_node("/root/UpgradeShop/UILayer")
 @onready var task_id = int(str(name))
 @onready var task_status = GM.tasks[int(task_id)]["index"]
@@ -23,7 +24,8 @@ func _ready() -> void:
 		
 func _pressed() -> void:
 	task_status = GM.tasks[int(task_id)]["index"]
-	UIlayer.open_inspector_task(task_name, task_description, task_id, task_status)
+	var final_task_desc = task_description + "\n\n" + task_to_use
+	UIlayer.open_inspector_task(task_name, final_task_desc, task_id, task_status)
 	item_status_clicked.visible = true
 	timer.start()
 	
